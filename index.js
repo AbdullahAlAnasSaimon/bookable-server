39060294975c3dab932941fb7899eb989ff2d9b7
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-20 23:01:55 +0600

    setup default getway and app listner

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
+const run = async () => {
+  try{}
+  finally{}
+}
+
+run().catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.status(400).send("Running Server")
+})
+
+app.listen(port, () => {
+  console.log("Server running on port", port);
+})
