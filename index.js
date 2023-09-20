7eb17b2ac54f1911a6eff26094a6bcdf823f20e0
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-20 23:05:05 +0600

    added nodemon script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
