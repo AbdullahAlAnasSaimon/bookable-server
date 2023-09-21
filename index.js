79d3039177f2ce3accfcc2da780407e57f7802af
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-21 22:12:33 +0600

    creating database

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ const run = async () => {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const serviceCollection = client.db("bookable").collection("products");
+    
   }
   finally{}
 }
