f7a10e6826ff9d2c413cf9c2b48b4548749caf90
abdullah <abdullahalanassaimon.swd@gmail.com>
2023-09-23 21:11:40 +0600

    find the searched and filterd product

--- index.js
+++ index.js
@@ -15,41 +15,100 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 const run = async () => {
-  try{
+  try {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
     const serviceCollection = client.db("bookable").collection("products");
 
     app.get("/products", async (req, res) => {
-      	try{
+      try {
         const query = {};
         const cursor = serviceCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
-        }
-        finally{
+      } finally {
+      }
+    });
 
-        }
-    })
+    // API endpoint for searching books
+    app.get("/books", async (req, res) => {
+      // const regex = new RegExp(query, "i");
+      const queries = req.query;
+      console.log(queries);
 
+      const modifiedQueries = {};
+      Object.entries(queries).forEach((query) => {
+        modifiedQueries[query[0]] = query[1];
+      });
 
+      const { searchTerm, genre, publicationYear } = modifiedQueries;
+
+      let andCondition = [];
+
+      if (searchTerm) {
+        andCondition.push({
+          $or: [
+            {
+              name: {
+                $regex: searchTerm,
+                $options: "i",
+              },
+            },
+            {
+              seller_name: {
+                $regex: searchTerm,
+                $options: "i",
+              },
+            },
+            {
+              genre: {
+                $regex: searchTerm,
+                $options: "i",
+              },
+            },
+          ],
+        });
+      }
+
+      if (genre) {
+        andCondition.push({
+          $and: [{ genre: { $regex: genre, $options: "i" } }],
+        });
+      }
+
+      if (publicationYear) {
+        andCondition.push({
+          $and: [
+            { publicationDate: { $regex: publicationYear, $options: "i" } },
+          ],
+        });
+      }
+
+      const query = andCondition.length > 0 ? { $and: andCondition } : {};
+
+      const result = await serviceCollection.find(query).toArray();
+
+      res.send({ status: true, data: result });
+    });
+  } finally {
   }
-  finally{}
-}
+};
 
-run().catch(err => console.log(err));
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.status(400).send("Running Server")
-})
+  res.status(400).send("Running Server");
+});
 
 app.listen(port, () => {
   console.log("Server running on port", port);
-})
+});
